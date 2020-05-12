0fef8173c804d52478f3d7e473a1d5ba9d15c05d
chengwei <chengwei@rlsk.link>
2020-05-12 10:37:55 +0800

    更新

---
+++ api_platform/PactVerify.py
@@ -0,0 +1,20 @@
+"""
+参考契约测试，实现自定义的接口数据返回的校验功能
+"""
+class Matcher(object):
+
+    def __init__(self,ma):
+
+    pass
+
+class Like(object):
+    pass
+
+class EachLike(object):
+    pass
+
+class Term(object):
+    pass
+
+class Enum(object):
+    pass
